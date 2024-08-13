57d70609b1190f401d37a2932c57a17c73b23d41
Ajitsharma1006 <ajitsharma1006@gmail.com>
2024-08-14 02:16:40 +0530

    added next container and css file

---
+++ Full Stack Assignment/Assignment/class's work/node.js
@@ -0,0 +1,113 @@
+// console.log("hello techno");
+
+// alert("hello tr-4");
+
+// var let const
+
+
+// var firstvar = 20;
+// console.log(firstvar);
+
+//datatype----primitiv type , reference type data type 
+// primitive typr -- string , number, bollean, undefined, symbol, bigint
+// reference type -- array, object , data, null 
+
+// var a = "Rohan sharma 'i am full stack'developer'";
+// console.log(a);
+
+// var a = -20.11;
+// console.log(a);
+
+// var str = "hello "
+// console.log(str);
+// console.log(typeof str);
+// var a = undefine;
+// console.log( typeof a);
+
+// var a =[10,38,40,85,89,20,30,]
+// var a =[10,38, true,"ajit",undefined,[],[],{}];
+// console.log(a);
+// console.log(typeof a);
+// var a = {
+//     name : "rohan",
+//     age : 25,
+//     city : "delhi"
+// }
+//     console.log(a);
+
+    //[{},{},{}]-- json--js object notation
+    //{[]}
+    // var date = new date();
+    // console.log(typeof date);
+    // var a = null;
+    // console.log(typeof a);
+
+    //Ecma script
+    //2015------ES06
+    // var b = 20;
+    // var b = 40;
+
+    // varibal with var can be redecleared or can be re assigned
+    // let c = 100;
+    // c = 200;
+    // variable with let cannot be redecleared but can be re assigned
+    // const d =1000;
+    //d=2000;
+    // variable with const cannot be redecleared or can be re assigned
+    // const buffer = Buffer.from('hello ,World!');
+    // console.log(buffer.toString('utf8'));
+
+
+// Operators
+// Arithematic Operator
+
+// let a = 10;
+// let b = 20;
+// let sum = a+b;
+
+// let sub =a-b;
+// let mul = a*b;
+// let div = a/b;
+// let exp = a ** b;
+// let rem = a % b;
+// console.log(sum,sub,mul,div ,div,exp,rem);
+
+//Addition Assigment
+// let x = 5;
+
+
+// x += 20;   //x = x+20; --addition Assignement
+// x -= 20;   //x = x+20; --sub Assignement
+// x *= 20;   //x = x+20; --mul Assignement
+// x /= 20;   //x = x+20; --div Assignement
+// x **= 20;   //x = x+20; -exp(power) Assignement
+// x %= 20;   //x = x+20; --Rem Assignement
+
+// console.log(x);
+
+// Comparision Operater
+let num1 = 30;
+let num2 = "30";
+//console.log(num1 >num2);
+// console.log(num1 === +num2);// type corsion
+console.log(num1 !=num2);// type corsion
+
+
+console.log(5-'5');
+console.log(5*'5');
+
+// let firstname = "ajabab";
+// let lastname = "dkd";
+// console.log(Firstname+"" +lastname);
+
+// Logical Opertators
+// let isLoggedIn = true;
+// let isAdmin = false;
+// console.log(isLoggedIn && IsAdmin);
+// console.log(isLoggedIn || IsAdmin);
+let isLoggedIn = 10;
+let isAdmin = 20;
+console.log(isLoggedIn && isAdmin);
+console.log(isLoggedIn || isAdmin);
+
+
