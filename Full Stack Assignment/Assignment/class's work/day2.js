0595ff138420df9424c5d9bfb3257cffdeaf0025
Ajitsharma1006 <ajitsharma1006@gmail.com>
2024-08-14 14:05:20 +0530

    day2 training class

---
+++ Full Stack Assignment/Assignment/class's work/day2.js
@@ -0,0 +1,103 @@
+
+
+// let count = 20;
+// if(count === 20){
+//     console.log("Count is equal to 20");
+
+// }
+// else{
+//     console.log("Count is not equal to 20");
+// }
+
+
+// let count = 20; 
+
+//     if(count === 10){
+//         console.log("Count is  10");
+//     }
+//     else if(count > 10){
+//         console.log("Count is grater 10");
+//     }
+//     else{
+//         console.log("Count is less 10");
+//     }
+
+
+//     let firstnum = prompt("Enter the first number");
+//     console.log(firstnum);
+// .....................................
+//prompt the user to enter their username and password
+
+    // let userName =prompt("enter user");
+    // let password = +prompt("enter password");
+    // console.log(name);
+    // console.log(password);
+    // if(name === "admin" && password === 123){
+    //     console.log("Login successful");
+    // }
+    // else{
+    //     console.log("Login failed");
+    // }
+    // let name = "admin";
+    // let password = "password";
+    // if(name === "admin" && password === "password"){
+    //     console.log("Login successful");
+    // }
+    // else{
+    //     console.log("Login failed");
+    // }
+
+    // if(userName === "admin"){
+    //     if(password === 123){
+    //         console.log("Login successful");
+    //     }
+    //     else{
+    //         console.log("Login failed");
+    //     }
+    // }
+    // else{
+    //     console.log("Invalid username");
+    // }
+
+    // //Ternary operator
+    // (userName==="admin" && password ===123)? console.log("auth successful"): console.log ("auth Failed");
+
+
+    // count==20?console.log("The count is 20") : count>20?
+    // console.log("The count is greater than 20") : console.log("The count is less than 20");
+
+    // if(count === 20){
+    //     console.log("The count is  20");
+    // }
+    // count ===20? console.log("The count is 20");
+
+    //----------------------------------------------------------------------switch case
+    // let day = 3
+    // switch(day){
+    //    case 1:
+    //                 day= "Monday"; break;
+    //     case 2 :
+    //          day="Tuesday"; break;
+    //     case 3 : 
+    //     day="Wednesday"; break;
+    //     case 4 : 
+    //     day="Thursday"; break;
+    //     case 5 : 
+    //     day="Friday";
+            
+    //         break;
+    //     case 6 : 
+    //     day="Saturday"; break;
+    //     case 7 :
+    //          day="Sunday"; break;
+            
+    //     default:
+    //         console.log("Invalid day");
+    // }
+    // console.log(day);
+//=---------------------------------------------------Function decleration
+// function sum(){
+//     console.log("this is sum function");
+// }
+// sum();
+
